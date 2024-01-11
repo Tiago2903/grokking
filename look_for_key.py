70cdf69f3c4336fc03fbceb6b2542e6ef5f865b2
tiago <xaloaxolot@gmail.com>
2024-01-11 17:48:40 -0300

    pseudocode

---
+++ look_for_key.py
@@ -0,0 +1,18 @@
+# pseudocode, more perfomatic
+def look_for_key(main_box):
+    pile = main_box.make_a_pile_to_look_through()
+    while pile is not empty:
+        box = pile.grab_a_box()
+        for item in box:
+            if item.is_a_box:
+                pile.append(item)
+            elif item.is_a_key():
+                print("found the key")
+                
+# pseudocode, more readable
+def look_for_key:
+    for item in box:
+        if item.is_a_box():
+            look_for_key(item) # function calls itself
+        elif item.is_a_key():
+                print("found the key")
